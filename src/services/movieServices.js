dcc75663ccd88c43ba730bf6ecc2303fec0c5de5
Elina_Sharlai <--elinakometa@gmail.com>
2025-06-02 14:41:23 +0300

    fixed bug

--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -1,28 +1,29 @@
 import axios from 'axios';
-
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-const API_AUTH = import.meta.env.VITE_API_AUTH;
 
-if (!API_AUTH) {
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+if (!API_KEY) {
     throw new Error(
-        'API key is missing. Please set API_AUTH in your environment variables.'
+        'API key is missing. Please set API_KEY in your environment variables.'
     );
 }
 const options = {
     params: {
+        api_key: API_KEY,
         language: 'en-US',
     },
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${API_AUTH}`,
     },
 };
 
 export const fetchTrending = async () => {
     const response = await axios.get('trending/movie/day', {
         params: options.params,
-        headers: options.headers,
+
     });
     return response.data;
 };
@@ -31,12 +32,11 @@ export const fetchSearch = async (query, page = 1) => {
     const response = await axios
         .get('search/movie', {
             params: {
-                ...options.params,
-                query: query,
-                include_adult: 'false',
+                api_key: API_KEY,
+                language: 'en-US',
                 page: page,
             },
-            headers: options.headers,
+
         })
         .then(response => response.data)
         .catch(error => console.error(error));
@@ -46,8 +46,11 @@ export const fetchSearch = async (query, page = 1) => {
 export const fetchMovieInfo = async movieId => {
     const response = await axios
         .get(`movie/${movieId}`, {
-            params: options.params,
-            headers: options.headers,
+            params: {
+                api_key: API_KEY,
+                language: 'en-US',
+                append_to_response: 'videos,images',
+            },
         })
         .then(response => response.data)
         .catch(error => console.error(error));
@@ -57,8 +60,11 @@ export const fetchMovieInfo = async movieId => {
 export const fetchMovieCast = async movieId => {
     const response = await axios
         .get(`movie/${movieId}/credits`, {
-            params: options.params,
-            headers: options.headers,
+            params: {
+                api_key: API_KEY,
+                language: 'en-US',
+                append_to_response: 'videos,images',
+            },
         })
         .then(response => response.data)
         .catch(error => console.error(error));
@@ -69,10 +75,10 @@ export const fetchMovieReviews = async (movieId, page = 1) => {
     const response = await axios
         .get(`movie/${movieId}/reviews`, {
             params: {
-                ...options.params,
+                api_key: API_KEY,
+                language: 'en-US',
                 page: page,
             },
-            headers: options.headers,
         })
         .then(response => response.data)
         .catch(error => console.error(error));
